c5ce9f02197416a691f07ef657c4813a45d386d3
Lek <LekPre19@htl-shkoder.com>
2026-06-13 17:41:50 +0200

    Implement Member class with member information and getters

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Member {
+    private String memberId;
+    private String name;
+    private String email;
+
+    public Member(String memberId, String name, String email) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Member ID: " + memberId +
+                ", Name: " + name +
+                ", Email: " + email;
+    }
 }
