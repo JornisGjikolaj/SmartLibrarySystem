f80a044ec93a488ce1bfc11de9e08c8d733e8d23
JornisGjikolaj <jornisgjikolaj@gmail.com>
2026-06-13 04:16:32 -0700

    Setup base Library structure and Main class

--- src/service/Library.java
+++ src/service/Library.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Book;
+import model.Member;
+import model.Loan;
+
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books = new ArrayList<>();
+    private ArrayList<Member> members = new ArrayList<>();
+    private ArrayList<Loan> loans = new ArrayList<>();
+
 }
